d17fa2a76e6e874dc41e6dedb97308df81652db9
Gabriel Nützi <gnuetzi@gmail.com>
2024-08-13 21:41:52 +0200

    fix: add option exercise

---
+++ exercises/options/src/bin/04.rs
@@ -0,0 +1,11 @@
+// TODO: Extend `grades` with `grade` without using `push`.
+//       Why does it work?
+fn main() {
+    // Extend a collection
+    let grade: Option<&str> = Some("A+");
+    let mut grades: Vec<&str> = vec!["B-", "C+", "D"];
+
+    todo!();
+
+    println!("{grades:?}")
+}
