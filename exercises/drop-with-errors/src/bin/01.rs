02b0c86cfab47f085b4e6057f6d633c0e6032b23
Gabriel Nützi <gabriel.nuetzi@sdsc.ethz.ch>
2025-01-24 12:46:52 +0100

    feat: add solution to drop-with-errors

--- exercises/drop-with-errors/src/bin/01.rs
+++ exercises/drop-with-errors/src/bin/01.rs
@@ -1,38 +1,34 @@
-// This exercise practices to implement `Drop` on a type while retaining error
-// handling.
+// This exercise practices to implement Drop on a type
+// while retaining error handling.
+// Inspiration from: Ch.3, p. 46 in "Rust for Rustaceans".
 //
-// We have a type `TempFile` which should simulate some resource which you acquire
-// and initialize with `new` (RAII) and which implements `Drop` which will remove
-// the file from the disc whenever it gets dropped.
+// We have a type `TempFile` which should simulate some resource
+// which you acquire and initialize with `new` (RAII)
+// and which implements `Drop` which will remove the
+// file from the disc whenever it gets dropped.
 //
-// Returning errors from `fn Drop::drop(&mut self)` is not possible. We could
-// ignore errors, but in this exercise we want to **leave no loose** ends and not
-// just ignore errors. That is why we implement an explicit destructor `close`
-// which takes ownership by taking `self` as value and returns
-// `Result<(), ErrorHandle>` in case something happens.
+// Returning errors from `Drop::drop` is not possible.
+// We could ignore errors, but in this exercise we want to leave no loose ends
+// and not just ignore errors.
+// Thats why we implement an explicit destructor `close` which
+// takes owner ship by taking `self` as value and returns
+// `Result<(), ErrorTempFile>` in case something happens.
 //
-// The code below compiles, but if you try to call `remove` in `close` the compiler
-// will tell you that you cannot partially move out values before `Drop::drop` is
-// called at function end. Also calling `remove` in `Drop` does not work because
-// `Drop::drop` does not own `self` it only has a `&mut self`.
+// The code below compiles, but if you try to call `remove` in
+// `close` the compiler will tell you that you cannot partially move
+// out values before `Drop::drop` is called at function end. Also calling `
+// `remove` in `Drop` does not work because `Drop::drop` does not own `self` it
+// only has a `&mut self`.
 //
 // The exercise is to solve these issues by wrapping `TempFile` into an `Option`
-// and implementing `Drop::drop` on the wrapped type leveraging the function
-// `Option<T>::take`.
-//
-// This exercise was inspired from advice in ch. 3, p. 46 in `Rust for Rustaceans`
-// from _J. Gjenset_.
 
 mod module {
-
-    // Simulating a complex object, a file handle,
-    // NOTE: Do not change this type.
     #[derive(Debug)]
     struct Handle(String);
 
     // Define our simple file resource RAII type.
     #[derive(Debug)]
-    pub struct TempFile {
+    struct TempFileImpl {
         file_handle: Handle,
     }
 
@@ -41,41 +37,49 @@ mod module {
         Remove,
     }
 
-    // A external function given from a library
-    // simulating the removal of this file handle.
-    // This takes ownership and removes the file on disk (virtually).
-    // NOTE: Do not change the signature of this function.
+    #[derive(Debug)]
+    pub struct TempFile(Option<TempFileImpl>);
+
     fn remove(file_handle: Handle) -> Result<(), ErrorFileHandle> {
         println!("Closing '{:?}'", &file_handle);
-        if file_handle.0 == "internal" {
+
+        if file_handle.0 == "internal-handle" {
             Err(ErrorFileHandle::Remove)
         } else {
             Ok(())
         }
     }
 
+    impl TempFileImpl {
+        pub fn close(self) -> Result<(), ErrorFileHandle> {
+            remove(self.file_handle)
+        }
+    }
+
     impl TempFile {
         pub fn new(file: impl AsRef<str>) -> TempFile {
-            TempFile {
+            TempFile(Some(TempFileImpl {
                 file_handle: Handle(file.as_ref().to_owned() + "-handle"),
-            }
+            }))
         }
 
-        // Close is an explicit destructor which takes ownership
-        // and closes the temporary file. It will directly remove
-        // the file from disk.
-        pub fn close(self) -> Result<(), ErrorFileHandle> {
-            // remove(self.file_handle)
-            Ok(())
+        pub fn close(mut self) -> Result<(), ErrorFileHandle> {
+            self.close_internal()
+        }
+
+        // Private function not for internal use only.
+        fn close_internal(&mut self) -> Result<(), ErrorFileHandle> {
+            match self.0.take() {
+                Some(f) => f.close(),
+                _ => Ok(()),
+            }
         }
     }
 
     impl Drop for TempFile {
-        // Thats the implicit destructor for TempFile,
-        // whenever this is destroyed.
         fn drop(&mut self) {
             println!("Dropping '{:?}'", &self);
-            // remove(self.file_handle)
+            let _ = self.close_internal();
         }
     }
 }
