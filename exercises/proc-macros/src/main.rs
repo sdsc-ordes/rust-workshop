c12e28319f5d7465950630ed03d5e3f6d09b68b4
Gabriel Nützi <gabriel.nuetzi@sdsc.ethz.ch>
2025-05-23 20:10:16 +0200

    fix: proc macros

--- exercises/proc-macros/src/main.rs
+++ exercises/proc-macros/src/main.rs
@@ -3,7 +3,22 @@
 use lib_macro::comp;
 
 fn main() {
-    // TODO: This should at the end become a real python comprehension.
+    println!("Comprehension: ");
     let v = vec![1, 2, 3];
-    comp![v[0]];
+    for i in comp![ x*x for x in v ] {
+        println!("{i}")
+    }
+
+    println!("Comprehension with if: ");
+    let v = 1..=10;
+    for i in comp![ x*x for x in v if x >= 3 if x <= 7 ] {
+        println!("{i}")
+    }
+
+    // That should not compile.
+    // println!("Comprehension with if (should not parse): ");
+    // let v = 1..=10;
+    // for i in comp![ x*x for x in v if x >= 3 && if x <= 7 ] {
+    //     println!("{i}")
+    // }
 }
